40c30e10d8236ed677a39226025afb8d59821320
emmanuel martinez <martinezemmanuel40@gmail.com>
2019-04-17 19:37:31 -0500

    exercies now get added to a list and json structure is partly made

--- app/views/CreatePlanScreen.js
+++ app/views/CreatePlanScreen.js
@@ -22,9 +22,59 @@ import {
   ListItem,
   Text
 } from "native-base";
-import CustomButton from '../components/customButton';
+import CustomButton from "../components/customButton";
+import { db } from "../src/config";
+
+// separate list so we keep UI and state components separate
+// both should display same data though
+// var listOfExercises = [
+//   {
+//     name: "Arm Extensions",
+//     days: "M W F"
+//   }
+// ];
+
+var workOutPlan = {
+  planName: "",
+  listOfExercises: []
+};
+
+var planName = "";
+var exerciseName = "";
+
+let setCurrentPlan = plan => {
+  //db.ref("/currPlan").remove();
+  // db.ref("/currPlan").push({
+  //   name: plan
+  // });
+  db.ref("/planList").push({
+    planName: planName
+  });
+};
 
 export default class CreatePlanScreen extends Component {
+  //this is where the nested plan structure will be located to save a plan to firebase
+  state = {
+    planName: "",
+    listOfExercies: [
+      {
+        name: "",
+        days: ""
+      }
+    ]
+  };
+
+  addExerciseToList() {
+    workOutPlan.planName = currPlanName;
+    workOutPlan.listOfExercises.push({
+      name: exerciseName,
+      days: "M W F"
+    });
+    console.log(workOutPlan);
+    this.forceUpdate();
+    //this.props.navigation.push("AddExerciseScreen");
+  }
+
   static navigationOptions = ({ navigation }) => ({
     header: (
       <Header style={{ backgroundColor: "#1d2731" }}>
@@ -46,45 +96,57 @@ export default class CreatePlanScreen extends Component {
   });
 
   render() {
+    //TODO move this to its own function and stuff
+    var exerciseList = [];
+    var i = 0;
+    workOutPlan.listOfExercises.forEach(exercise => {
+      exerciseList.push(
+        <ListItem key={i++}>
+          <Body>
+            <Text>{exercise.name}</Text>
+          </Body>
+
+          <Right>
+            <Text>{exercise.days}</Text>
+          </Right>
+        </ListItem>
+      );
+    });
+
     return (
       <Container>
-        
         <Content style={styles.content} contentContainerStyle={{ flexGrow: 1 }}>
           <Form>
             <Item inlineLabel>
               <Label>Plan Name</Label>
-              <Input />
+              <Input onChangeText={name => (currPlanName = name)} />
             </Item>
 
             <Item inlineLabel>
-            <Label>Exercise Name</Label>
-            <Input/>
-            <Button transparent onPress = {() => { this.props.navigation.push("AddExerciseScreen"); }}>
-              <Text>
-                Add
-              </Text>
-            </Button>
+              <Label>Exercise Name</Label>
+              <Input onChangeText={name => (exerciseName = name)} />
+              <Button
+                transparent
+                onPress={() => {
+                  this.addExerciseToList();
+                  //this.props.navigation.push("AddExerciseScreen");
+                }}
+              >
+                <Text>Add</Text>
+              </Button>
             </Item>
           </Form>
-          <Text style={ styles.text }>Selected Exercises: </Text>
-          <List>
-            <ListItem>
-              <Body>
-                <Text>Arm Extensions</Text>
-              </Body>
-              
-              <Right>
-                <Text>M T W Th F S Su</Text>
-              </Right>     
-            </ListItem>
-          </List>
+          <Text style={styles.text}>Selected Exercises: </Text>
+          <List>{exerciseList}</List>
         </Content>
 
-        <Footer style={{backgroundColor: "#0b3c5d"}}>
-        <CustomButton 
-          text    = "Save Program"
-          onPress = {() => { alert("save program") }}
-        />
+        <Footer style={{ backgroundColor: "#0b3c5d" }}>
+          <CustomButton
+            text="Save Program"
+            onPress={() => {
+              alert("save program");
+            }}
+          />
         </Footer>
       </Container>
     );
@@ -93,14 +155,13 @@ export default class CreatePlanScreen extends Component {
 
 const styles = StyleSheet.create({
   content: {
-    flex           : 1,
-    backgroundColor: "#ffffff",
+    flex: 1,
+    backgroundColor: "#ffffff"
   },
-  text:{
-    alignItems: 'center',
-    marginTop : 20,
+  text: {
+    alignItems: "center",
+    marginTop: 20,
     marginLeft: 12,
-    fontSize  : 22
+    fontSize: 22
   }
-  
 });
